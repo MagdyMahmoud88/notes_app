e858efad9f5f9eab74022e47c065bff26d3bcabf
MagdyMahmoud88 <magdymahmoud8812@gmail.com>
2023-03-18 12:44:31 +0200

    create custom appBar

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_app_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            CustomAppBar(),
+          ],
+        ),
+      ),
+    );
   }
 }
